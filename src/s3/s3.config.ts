d5efdddc27e86a068c4e887fd16224eb7da0b0f5
Laurent Ouma <lawiomosh3@gmail.com>
2026-03-13 20:31:09 +0300

    feat: Add S3 public endpoint configuration and implement presigner client for generating signed URLs in S3 service

--- src/s3/s3.config.ts
+++ src/s3/s3.config.ts
@@ -13,6 +13,11 @@ export class S3Config {
     default: 'http://localhost:9000',
   })
   endpoint: string;
+  @Value('S3_PUBLIC_ENDPOINT', {
+    parse: z.url().parse,
+    default: 'http://localhost:9000',
+  })
+  publicEndpoint: string;
   @Value('S3_BUCKET_TMP', {
     parse: z.string().parse,
     default: 'cl-tmp',
