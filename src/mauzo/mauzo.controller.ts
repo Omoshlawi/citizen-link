d7d2959c6d2934115ad2ddcd919c3f63de6ad79a
Laurent Ouma <lawiomosh3@gmail.com>
2026-05-16 23:49:25 +0300

    refactor(postgres-init): remove deprecated app user creation script

--- src/mauzo/mauzo.controller.ts
+++ src/mauzo/mauzo.controller.ts
@@ -2,11 +2,13 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { MauzoWebHookService } from './mauzo.webhook.service';
 import { MauzoWebhookSignatureGuard } from './mauzo.guard';
 import { WebHookDto } from './mauzo.dto';
+import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 
 @Controller('mauzo')
 export class MauzoController {
   constructor(private readonly mauzoWebhookService: MauzoWebHookService) {}
 
+  @AllowAnonymous()
   @UseGuards(MauzoWebhookSignatureGuard)
   @Post('event')
   onPaymentEvent(@Body() paymentEventDto: WebHookDto) {
