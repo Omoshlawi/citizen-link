ba5ef75d4b33fffa68b5f6b490ebc5d3edb2dd47
Laurent Ouma <lawiomosh3@gmail.com>
2026-05-30 11:44:11 +0300

    refactor(env): rename AI_SERVICE_URL and AI_SERVICE_INTERNAL_SECRET to CHATBOT_SERVICE_URL and CHATBOT_SERVICE_INTERNAL_SECRET for clarity and consistency

--- src/chat-bot/chat-bot.config.ts
+++ src/chat-bot/chat-bot.config.ts
@@ -4,15 +4,16 @@ import z from 'zod';
 
 @Configuration()
 export class ChatBotConfig {
-  @Value('AI_SERVICE_URL', {
-    parse: z.url({ message: 'Invalid AI_SERVICE_URL' }).parse,
+  @Value('CHATBOT_SERVICE_URL', {
+    parse: z.url({ message: 'Invalid CHATBOT_SERVICE_URL' }).parse,
   })
   baseUrl!: string;
 
-  @Value('AI_SERVICE_INTERNAL_SECRET', {
+  @Value('CHATBOT_SERVICE_INTERNAL_SECRET', {
     parse: z
       .string()
-      .min(16, 'AI_SERVICE_INTERNAL_SECRET must be at least 16 chars').parse,
+      .min(16, 'CHATBOT_SERVICE_INTERNAL_SECRET must be at least 16 chars')
+      .parse,
   })
   internalSecrete!: string;
 }
